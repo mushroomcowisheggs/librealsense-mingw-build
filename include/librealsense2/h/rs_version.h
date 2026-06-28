0cff8443b3c0ec1cd9649b4880462b23c5091597
Mushroomcowisheggs <egg_mushroomcow@foxmail.com>
2026-06-28 15:09:18 +0800

    Add files via upload

---
+++ include/librealsense2/h/rs_version.h
@@ -0,0 +1,10 @@
+// include/librealsense2/h/rs_version.h
+#ifndef LIBREALSENSE_RS_VERSION_H
+#define LIBREALSENSE_RS_VERSION_H
+
+#define RS2_API_MAJOR_VERSION    2
+#define RS2_API_MINOR_VERSION    58
+#define RS2_API_PATCH_VERSION    1
+#define RS2_API_BUILD_VERSION    0
+
+#endif // LIBREALSENSE_RS_VERSION_H
