43043aeb831aa2c042589f5a55eae70720b8fc82
Mushroomcowisheggs <egg_mushroomcow@foxmail.com>
2026-06-28 15:08:43 +0800

    mingw build

---
+++ src/res/winres_constants.h
@@ -0,0 +1,13 @@
+// src/res/winres_constants.h
+// Minimal Windows resource constants for Librealsense RC files
+
+#ifndef _WINRES_CONSTANTS_H
+#define _WINRES_CONSTANTS_H
+
+// Language IDs
+#define LANG_NEUTRAL                    0x00
+#define SUBLANG_NEUTRAL                 0x00
+#define LANG_ENGLISH                    0x09
+#define SUBLANG_ENGLISH_US              0x01
+
+#endif
